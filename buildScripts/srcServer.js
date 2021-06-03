6453c3fdc5cda84c821294d0a930582012c77211
Morgan Powers <morganpowers@Morgans-MacBook-Pro-3.local>
2021-06-03 14:41:48 -0400

    fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity; pretends this hits a real database
+  res.json([
+    {"id": 1, "firstName":"Bob", "lastName":"Belcher", "email":"bobbelcher@gmail.com"},
+    {"id": 2, "firstName":"Tina", "lastName":"Belcher", "email":"tinabelcher@gmail.com"},
+    {"id": 3, "firstName":"Louise", "lastName":"Belcher", "email":"louisebelcher@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
